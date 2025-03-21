0ba543e9aef2ef8b1eddc0aaaefb2a7c20e937a4
SamuelJunr <samuelgomespj@outlook.com>
2025-03-20 21:42:30 -0300

    esqueleto do projeto

---
+++ TesteApp/Core/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace TesteApp.Core
+{
+    internal class ObservableObject : INotifyPropertyChanging
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+
+        protected void NotifyPropertyChanging([CallerMemberName] string? Name = null)
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(Name));
+        }
+    }
+
+
+}
